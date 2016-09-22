c5155ebb99f336b89774f21eeb9d0cf14688e27c
Chase Wasden <ccwasden@gmail.com>
2016-09-21 18:12:24 -0600

    Revert "removed underscore.. total guess"
    This reverts commit 86629601570ee6951c59ef8de9bf944914eb1653.

--- PureLayout/PureLayoutTests/PureLayoutTestBase.m
+++ PureLayout/PureLayoutTests/PureLayoutTestBase.m
@@ -10,7 +10,7 @@
 
 @implementation PureLayoutTestBase
 
-- (PL_NSArray_of(ALView *) *)viewArray
+- (PL__NSArray_of(ALView *) *)viewArray
 {
     return @[self.viewA, self.viewB, self.viewC, self.viewD];
 }
